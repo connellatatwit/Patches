52da346e460d11e4c027d26a8edfa3c1a3b52274
Connella <connellat@wit.edu>
2023-03-19 21:33:51 -0400

    Blue Dog
    Gofd

--- Assets/Scripts/EnemyFollowPlayer.cs
+++ Assets/Scripts/EnemyFollowPlayer.cs
@@ -9,6 +9,8 @@ public class EnemyFollowPlayer : MonoBehaviour
     [SerializeField] float minDist = .1f;
     [SerializeField] float maxDist = .2f;
 
+    [SerializeField] SpriteRenderer sr;
+
     private Rigidbody2D rb;
 
     private void Start()
@@ -22,5 +24,14 @@ public class EnemyFollowPlayer : MonoBehaviour
     {
         Vector3 dir = player.position - transform.position;
         rb.velocity = dir.normalized * eS.Speed() * Time.deltaTime;
+
+        if(rb.velocity.x > 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
     }
 }
