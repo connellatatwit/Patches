0e1deb06a79546299d57fe5e408cc7d669085b43
connellatatwit <connellat@wit.edu>
2023-03-24 12:15:35 -0400

    This wont work
    Wont work

--- Assets/Scripts/BgLoop.cs
+++ Assets/Scripts/BgLoop.cs
@@ -23,5 +23,38 @@ public class BgLoop : MonoBehaviour
         float objWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x;
         int childNeeded = (int)Mathf.Ceil(screeBounds.x * 2 / objWidth);
         GameObject clone = Instantiate(obj) as GameObject;
+        for (int i = 0; i <= childNeeded; i++)
+        {
+            GameObject c = Instantiate(clone) as GameObject;
+            c.transform.SetParent(obj.transform);
+            c.transform.position = new Vector3(objWidth * i, obj.transform.position.y, obj.transform.position.z);
+            c.name = obj.name + i;
+        }
+        Destroy(clone);
+        Destroy(obj.GetComponent<SpriteRenderer>());
+    }
+
+    void Reposition(GameObject obj)
+    {
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+        if(children.Length > 1)
+        {
+            GameObject firstChild = children[1].gameObject;
+            GameObject lastChild = children[children.Length - 1].gameObject;
+
+            float halfWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+            if(transform.position.x + screeBounds.x > lastChild.transform.position.x + halfWidth)
+            {
+                firstChild.transform.SetAsLastSibling();
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfWidth * 2, lastChild.transform.position.y, lastChild.transform.position.z);
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        foreach (GameObject item in levels)
+        {
+            Reposition(item);
+        }
     }
 }
