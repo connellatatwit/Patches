578771a2a6d1f8bf376e5cb4648ae6bcb869efcb
Connella <connellat@wit.edu>
2023-04-08 01:21:48 -0400

    Regen
    Regen

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -13,11 +13,25 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] GameObject loseScreen;
     [SerializeField] Timer clock;
 
+    [SerializeField] int healthRegen;
+    [SerializeField] float regenCd;
+    private float regenTimer;
+
     private void Start()
     {
         currentHealth = baseMaxHealth;
         maxHealth = baseMaxHealth;
         hpBar.fillAmount = (float)currentHealth / (float)baseMaxHealth;
+        regenTimer = regenCd;
+    }
+    private void Update()
+    {
+        regenTimer -= Time.deltaTime;
+        if(regenTimer <= 0)
+        {
+            Heal(healthRegen);
+            regenTimer = regenCd;
+        }
     }
 
     public void TakeDamage(int amount)
@@ -34,7 +48,15 @@ public class PlayerHealth : MonoBehaviour
 
         hpBar.fillAmount = (float)currentHealth / (float)maxHealth;
     }
-
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        if(currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        hpBar.fillAmount = (float)currentHealth / (float)maxHealth;
+    }
     public void IncreaseMaxHealth(int amount)
     {
         maxHealth += amount;
